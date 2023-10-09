6082fcc963148338dd409af46506e7a077f06605
mahmudul <imdmahmudul237@gmail.com>
2023-10-09 19:52:58 +0600

    update some style on todos page

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,44 @@
 <template>
-  <div class="max-w-[800px] mx-auto py-8">
-    <div class="text-center">
-      <h1
-        class="text-xl text-black border inline-block py-2 px-48 border-black rounded"
-      >
-        Full-Stack Todo
-      </h1>
-    </div>
-    <div class="mt-10 flex items-center gap-2">
-      <input
-        class="w-full py-2 px-2 border border-gray-500 rounded"
-        type="text"
-        name=""
-        id=""
-        placeholder="Insert Your Todo"
-      />
-      <button class="py-2 px-2 border border-gray-500 rounded">Submit</button>
-    </div>
-    <div>
-      <table class="w-full my-8">
-        <tr class="py-5 border-gray-500 border rounded w-full">
-          <td class="flex items-center py-2 px-1 title">
-            Todo 1
-            <div class="ml-auto">
-              <button class="border border-gray-400 py-1 px-3 rounded mr-1">
+  <div class="bg-[#475569] h-[100vh] flex items-center justify-center">
+    <div class="bg-[#0f172a] max-w-[600px] mx-auto p-8 rounded-md">
+      <div class="text-center bg-[#475569] py-2 px-48 rounded-full">
+        <h1 class="text-xl text-white inline-block">Full-Stack Todo</h1>
+      </div>
+      <div class="mt-10 flex items-center gap-2">
+        <input
+          class="w-full outline-none py-2 px-3 border border-gray-300 rounded-full"
+          type="text"
+          name=""
+          id=""
+          placeholder="Insert your todo"
+        />
+        <button
+          class="bg-[#a3e635] text-black border-none py-2 px-8 border rounded-full border-gray-500"
+        >
+          Submit
+        </button>
+      </div>
+      <div>
+        <ul class="pt-4">
+          <li
+            class="py-2 px-2 flex items-center justify-between border-gray-300 border rounded w-full"
+          >
+            <p class="text-white">todo</p>
+            <div class="flex items-center gap-1">
+              <button
+                class="bg-[#a3e635] text-sm text-black py-2 px-6 rounded-full"
+              >
                 Edit
               </button>
-              <button class="border border-gray-400 py-1 px-3 rounded">
+              <button
+                class="bg-[#EC4A9D] text-[white] text-sm py-2 px-6 rounded-full"
+              >
                 Delete
               </button>
             </div>
-          </td>
-        </tr>
-        <tr class="border-gray-500 py-5 border rounded w-full">
-          <td class="flex items-center py-2 px-1">
-            Todo 2
-            <div class="ml-auto">
-              <button class="border border-gray-400 py-1 px-3 mr-1 rounded">
-                Edit
-              </button>
-              <button class="border border-gray-400 py-1 px-3 rounded">
-                Delete
-              </button>
-            </div>
-          </td>
-        </tr>
-      </table>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
